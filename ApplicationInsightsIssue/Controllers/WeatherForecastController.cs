4174c0b0c5cfc12bd14518e32990951bfb668567
Ville Henriksson <ville.henriksson@if.fi>
2024-05-06 09:50:38 +0300

    Add project files.

---
+++ ApplicationInsightsIssue/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationInsightsIssue.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly TelemetryClient _telemetryClient;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, TelemetryClient telemetryClient)
+        {
+            _logger = logger;
+            _telemetryClient = telemetryClient;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
